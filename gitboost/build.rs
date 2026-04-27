32b29faed85857da45350dcb20573ed1dd3b357d
kjw2 <jaethis@gmail.com>
2026-04-27 04:34:40 +0000

    style: fix rustfmt line wrap in build.rs

--- gitboost/build.rs
+++ gitboost/build.rs
@@ -2,8 +2,8 @@ fn main() {
     // GITBOOST_GITHUB_CLIENT_ID를 컴파일타임 환경변수로 주입
     // 환경변수가 설정되지 않으면 기본 Client ID를 사용
     const DEFAULT_CLIENT_ID: &str = "Ov23liys1cnsX7VYUfIQ";
-    let client_id =
-        std::env::var("GITBOOST_GITHUB_CLIENT_ID").unwrap_or_else(|_| DEFAULT_CLIENT_ID.to_string());
+    let client_id = std::env::var("GITBOOST_GITHUB_CLIENT_ID")
+        .unwrap_or_else(|_| DEFAULT_CLIENT_ID.to_string());
     println!("cargo:rustc-env=GITBOOST_GITHUB_CLIENT_ID={}", client_id);
     println!("cargo:rerun-if-env-changed=GITBOOST_GITHUB_CLIENT_ID");
 }
